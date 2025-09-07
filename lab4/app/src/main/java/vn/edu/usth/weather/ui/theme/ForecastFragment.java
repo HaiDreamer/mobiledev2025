8bb8ab7144c568b394749c2b064596e3411f9137
HaiDreamer <nghuyhai2004@gmail.com>
2025-09-07 20:06:03 +0700

    done lab4

---
+++ lab4/app/src/main/java/vn/edu/usth/weather/ui/theme/ForecastFragment.java
@@ -0,0 +1,33 @@
+package vn.edu.usth.weather.ui.theme;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import vn.edu.usth.weather.R;
+
+public class ForecastFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        return v;
+    }
+}
+
+
